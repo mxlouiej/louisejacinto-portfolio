0794a83f8c1f2cc305b1f4f27398c28b33697a41
Louise Jacinto <donnalouise.jacinto@benilde.edu.ph>
2020-10-20 18:16:05 +0800

    added sections and swipe function

--- src/app/App.js
+++ src/app/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import SwipeableViews from "react-swipeable-views";
+import { bindKeyboard } from "react-swipeable-views-utils";
+
+import About from "../sections/About";
+import Contact from "../sections/Contact";
+import Home from "../sections/Home";
+import Works from "../sections/Works";
+
+const BindKeyboardSwipeableViews = bindKeyboard(SwipeableViews);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BindKeyboardSwipeableViews>
+        <Home />
+        <About />
+        <Works />
+        <Contact />
+      </BindKeyboardSwipeableViews>
     </div>
   );
 }
