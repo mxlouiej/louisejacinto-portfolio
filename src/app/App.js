eb006fa19bb3283b90f293bda823db0985f1338b
Louise Jacinto <donnalouise.jacinto@benilde.edu.ph>
2020-10-21 14:31:27 +0800

    added photo in about section

--- src/app/App.js
+++ src/app/App.js
@@ -24,7 +24,6 @@ function App() {
 
   return (
     <div className="App">
-      {console.log("currently on section:", section)}
       <BindKeyboardSwipeableViews onChangeIndex={(e) => showIndex(e)}>
         <Home />
         <About />
--- src/sections/About.js
+++ src/sections/About.js
@@ -1,11 +1,58 @@
 import React from "react";
+import { Avatar, Box, makeStyles } from "@material-ui/core";
+import graduate from "../assets/img/graduate.jpg";
 
 const About = () => {
+  const classes = useStyles();
   return (
-    <div>
-      <h1>About Section</h1>
-    </div>
+    <Box className={classes.about}>
+      <h1 className="about-text">Call me Louise</h1>
+      <Box className="about-container">
+        <Box className="description">{/* description here */}</Box>
+
+        <Box className="about-photo">
+          <Avatar alt="Grad Photo" src={graduate} className={classes.photo} />
+        </Box>
+      </Box>
+    </Box>
   );
 };
+
+const useStyles = makeStyles((theme) => ({
+  about: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    color: "#FFFF",
+    backgroundColor: "#000051",
+    paddingBottom: "2vh",
+
+    [theme.breakpoints.down("sm")]: {
+      backgroundColor: "red",
+      // smallest,
+    },
+    [theme.breakpoints.up("md")]: {
+      backgroundColor: "blue",
+      // medium,
+    },
+    [theme.breakpoints.up("lg")]: {
+      backgroundColor: "green",
+    },
+  },
+  photo: {
+    [theme.breakpoints.down("sm")]: {
+      height: theme.spacing(20),
+      width: theme.spacing(20),
+    },
+    [theme.breakpoints.up("md")]: {
+      height: theme.spacing(30),
+      width: theme.spacing(30),
+    },
+    [theme.breakpoints.up("lg")]: {
+      height: theme.spacing(40),
+      width: theme.spacing(40),
+    },
+  },
+}));
 
 export default About;
