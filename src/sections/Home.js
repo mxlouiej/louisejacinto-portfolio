a324ad9f3f259ecb6ad8692193b79f0533fed3cf
Louise Jacinto <donnalouise.jacinto@benilde.edu.ph>
2020-11-22 23:23:23 +0800

    moved home css

--- src/sections/Home.js
+++ src/sections/Home.js
@@ -5,8 +5,8 @@ const Home = () => {
   const homeStyles = useStyles();
   return (
     <Box className={homeStyles.landing}>
-      <h1 className="home-text">Well, hello there!</h1>
-      <p className="subtitle">
+      <h1 className={homeStyles.homeText}>Well, hello there!</h1>
+      <p className={homeStyles.subtitle}>
         Go swipe right (or use right arrow key) to view my portfolio 👉👉👉
       </p>
     </Box>
@@ -22,6 +22,22 @@ const useStyles = makeStyles((theme) => ({
     minHeight: "100vh",
     color: "#FFFF",
     backgroundColor: "#5C068C",
+  },
+  homeText: {
+    fontSize: "100px",
+    textAlign: "right",
+    alignSelf: "center",
+    fontFamily: "Alegreya, serif",
+    textShadow: "12px 7px #3f0361",
+    marginBottom: "0",
+  },
+  subtitle: {
+    color: "#ababab",
+
+    [theme.breakpoints.down("sm")]: {
+      //smallest
+      textAlign: "right",
+    },
   },
 }));
 
